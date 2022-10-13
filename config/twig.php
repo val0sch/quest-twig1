932c1352479e3985f228e273ff4611ead19477a8
Valerie <valerie.ielsch@gmail.com>
2022-10-14 00:57:55 +0200

    challenge ok

--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,21 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
+// Tu indiques ici à Twig où aller chercher tes fichiers de vues
+
+// Le loader (Twig\Loader\FilesystemLoader) demande un paramètre, 
+// qui sera le répertoire dans lequel tes fichiers de vues en *.twig se trouveront.
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+
+
+// TwigEnvironment prend ensuite cet objet loader en paramètre, 
+// et un tableau de configuration (cache, debug, etc.), 
+// à mettre optionnellement en second paramètre, mais tu ne t'en préoccuperas pas pour le moment.
 $twig = new Twig\Environment($loader, ['debug' => true]);
+
+//extension qui permet d'utiliser l'équivalent du var_dump(), soit : dump()  chez twig
+// ex : {{ dump(objectIWantExamine) }}
 $twig->addExtension(new Twig\Extension\DebugExtension());
+//liste des extensions dispos:
+// https://twig.symfony.com/doc/3.x/api.html#using-extensions
